23b993a0c1cd72b7692baf5b2830a1f19b30f6ac
Robert Wenzel <cor3000@web.de>
2017-04-12 00:35:10 +0800

    some ws inside express

--- server/static-server.js
+++ server/static-server.js
@@ -1,14 +1,27 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const helmet = require('helmet');
+const app = express();
 
-/*
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-})
-*/
+const expressWs = require('express-ws')(app);
 
-app.use(express.static('public'))
+//security recommended stuff
+app.disable('x-powered-by')
+app.use(helmet())
+
+//static resources
+app.use('/static', express.static('public'))
+
+//enable websocket
+app.ws('/ws', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+  console.log('socket', req);
+  console.log('ws', ws);
+});
+
 
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!');
 });
+
